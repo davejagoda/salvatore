d13522169aa90f987ed0b36a5389d607bbb3511d
Dave Jagoda <davejagoda@gmail.com>
2015-02-23 08:46:16 -0800

    add lsGoogleDrive.py

---
+++ lsGoogleDrive.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+
+import pprint
+import httplib2
+import apiclient.discovery
+import apiclient.http
+import oauth2client.client
+
+OAUTH2_SCOPE = 'https://www.googleapis.com/auth/drive'
+CLIENT_SECRETS = 'client_secrets.json'
+
+f = open('bearer_token.json', 'r')
+credentials = oauth2client.client.Credentials.new_from_json(f.read())
+f.close()
+print(type(credentials))
+print(credentials)
+
+http = httplib2.Http()
+credentials.authorize(http)
+drive_service = apiclient.discovery.build('drive', 'v2', http=http)
+
+result = []
+page_token = None
+while True:
+    param = {}
+    if page_token:
+        param['pageToken'] = page_token
+    files = drive_service.files().list(**param).execute()
+    result.extend(files['items'])
+    page_token = files.get('nextPageToken')
+    if not page_token:
+        break
+
+print(result)
+print(len(result))
