f2e9a3f2c949f236a0d650f88e2e52d24f24098c
Dave Jagoda <davejagoda@gmail.com>
2015-08-15 11:59:55 -0700

    modify lsGoogleDrive.py to use argparse

--- lsGoogleDrive.py
+++ lsGoogleDrive.py
@@ -1,32 +1,38 @@
 #!/usr/bin/env python
 
+import argparse
 import pprint
 import httplib2
 import apiclient.discovery
 import oauth2client.client
 
-f = open('bearer_token.json', 'r')
-credentials = oauth2client.client.Credentials.new_from_json(f.read())
-f.close()
-print(type(credentials))
-print(credentials)
+def listDrive(drive_service):
+    result = []
+    page_token = None
+    while True:
+        param = {}
+        if page_token:
+            param['pageToken'] = page_token
+        files = drive_service.files().list(**param).execute()
+        result.extend(files['items'])
+        page_token = files.get('nextPageToken')
+        if not page_token:
+            break
+    return(result)
 
-http = httplib2.Http()
-credentials.authorize(http)
-drive_service = apiclient.discovery.build('drive', 'v2', http=http)
-
-result = []
-page_token = None
-while True:
-    param = {}
-    if page_token:
-        param['pageToken'] = page_token
-    files = drive_service.files().list(**param).execute()
-    result.extend(files['items'])
-    page_token = files.get('nextPageToken')
-    if not page_token:
-        break
-
-print('total {}'.format(len(result)))
-for item in result:
-    pprint.pprint(item)
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-v', '--verbose', action='store_true', help='show verbose output')
+    parser.add_argument('-t', '--tokenFile', action='store', required=True, help='file containing OAuth token in JSON format')
+    args = parser.parse_args()
+    with open(args.tokenFile, 'r') as f:
+        credentials = oauth2client.client.Credentials.new_from_json(f.read())
+    if args.verbose:
+        print(type(credentials))
+        print(credentials)
+    http = httplib2.Http()
+    credentials.authorize(http)
+    result = listDrive(apiclient.discovery.build('drive', 'v2', http=http))
+    print('total {}'.format(len(result)))
+    for item in result:
+        pprint.pprint(item)
