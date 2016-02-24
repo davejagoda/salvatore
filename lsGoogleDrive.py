8c8ebbd79a73af8b8904a90b4b730c12136d72b5
Dave Jagoda <davejagoda@users.noreply.github.com>
2016-02-23 22:23:48 -0800

    allow lsGoogleDrive.py to print names only

--- lsGoogleDrive.py
+++ lsGoogleDrive.py
@@ -6,7 +6,14 @@ import httplib2
 import apiclient.discovery
 import oauth2client.client
 
-def listDrive(drive_service):
+def get_drive_service(tokenFile, verbose=0):
+    with open(tokenFile, 'r') as f:
+        credentials = oauth2client.client.Credentials.new_from_json(f.read())
+    http = httplib2.Http()
+    credentials.authorize(http)
+    return(apiclient.discovery.build('drive', 'v2', http=http))
+
+def list_drive(drive_service, verbose=0):
     result = []
     page_token = None
     while True:
@@ -20,19 +27,28 @@ def listDrive(drive_service):
             break
     return(result)
 
-if '__main__' == __name__:
-    parser = argparse.ArgumentParser()
-    parser.add_argument('-v', '--verbose', action='store_true', help='show verbose output')
-    parser.add_argument('-t', '--tokenFile', action='store', required=True, help='file containing OAuth token in JSON format')
-    args = parser.parse_args()
-    with open(args.tokenFile, 'r') as f:
-        credentials = oauth2client.client.Credentials.new_from_json(f.read())
-    if args.verbose:
-        print(type(credentials))
-        print(credentials)
-    http = httplib2.Http()
-    credentials.authorize(http)
-    result = listDrive(apiclient.discovery.build('drive', 'v2', http=http))
+def print_raw_result(result):
     print('total {}'.format(len(result)))
     for item in result:
         pprint.pprint(item)
+
+def print_names(result):
+    names = []
+    for item in result:
+        names.append(item['title'])
+    names.sort()
+    for name in names:
+        print(name.encode('utf8'))
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--tokenFile', action='store', required=True, help='file containing OAuth token in JSON format')
+    parser.add_argument('-r', '--raw', action='store_true', help='show raw output')
+    parser.add_argument('-v', '--verbose', action='count', help='show verbose output')
+    args = parser.parse_args()
+    drive_service = get_drive_service(args.tokenFile, args.verbose)
+    result = list_drive(drive_service, args.verbose)
+    if args.raw:
+        print_raw_result(result)
+    else:
+        print_names(result)
