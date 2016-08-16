05279c767198a35dae7c000825fc73875bae7794
dj <davejagoda@users.noreply.github.com>
2016-08-16 01:28:29 -0700

    add lsS3Buckets.py

--- lsS3Buckets.py
+++ lsS3Buckets.py
@@ -2,5 +2,9 @@
 
 import boto3
 s3 = boto3.resource('s3')
+
 for bucket in s3.buckets.all():
     print(bucket.name)
+    for obj in bucket.objects.all():
+        assert bucket.name == obj.bucket_name
+        print(' {}'.format(obj.key))
