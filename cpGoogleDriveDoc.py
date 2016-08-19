ae06a10b476808bd90af0b55b4de935d18825557
dj <davejagoda@users.noreply.github.com>
2016-08-19 14:37:01 -0700

    Combine cpGoogleDriveCSV.py and cpGoogleDriveDoc.py

--- cpGoogleDriveDoc.py
+++ cpGoogleDriveDoc.py
@@ -4,13 +4,18 @@ import argparse
 import apiclient
 from utils import get_drive_service
 
-def uploadDrive(drive_service, filename):
+mime_dict = {
+    'csv': ['application/vnd.google-apps.spreadsheet', 'text/csv'],
+    'doc': ['application/vnd.google-apps.document', 'text/plain']
+}
+
+def uploadDrive(drive_service, filename, dict_key):
     body = {
         'title': filename,
-        'mimeType': 'application/vnd.google-apps.document'
+        'mimeType': mime_dict[dict_key][0]
     }
     media_body = apiclient.http.MediaFileUpload(filename,
-                                 mimetype='text/plain',
+                                 mimetype=mime_dict[dict_key][1],
                                  resumable=True
     )
     return(drive_service.files().insert(body=body, media_body=media_body).execute()['id'])
@@ -19,7 +24,14 @@ if '__main__' == __name__:
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--tokenFile', action='store', required=True, help='file containing OAuth token in JSON format')
     parser.add_argument('-v', '--verbose', action='count', help='show verbose output')
+    group = parser.add_mutually_exclusive_group(required=True)
+    group.add_argument('-c', '--csv', action='store_true', help='convert to google spreadsheet')
+    group.add_argument('-d', '--doc', action='store_true', help='convert to google doc')
     parser.add_argument('filename', help='the path to the file to be uploaded')
     args = parser.parse_args()
     drive_service = get_drive_service(args.tokenFile, args.verbose)
-    print(uploadDrive(drive_service, args.filename))
+    if args.csv:
+        dict_key = 'csv'
+    if args.doc:
+        dict_key = 'doc'
+    print(uploadDrive(drive_service, args.filename, dict_key))
