520d7851325eba114b13fd4520d960ff879415ad
dj <davejagoda@users.noreply.github.com>
2017-06-26 15:35:15 -0700

    Add mvGoogleDriveBulk.py and get_folder_contents_from_id to utils

---
+++ mvGoogleDriveBulk.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python
+
+import argparse
+import apiclient
+import os
+import sys
+import utils
+
+if '__main__' == __name__:
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-t', '--tokenFile', required=True,
+                        help='file containing OAuth token in JSON format')
+    parser.add_argument('-v', '--verbose', action='count',
+                        help='show verbose output')
+    parser.add_argument('old_folder', help='the old folder')
+    parser.add_argument('new_folder', help='the new folder')
+    args = parser.parse_args()
+    drive_service = utils.get_drive_service(args.tokenFile, args.verbose)
+    old_id = utils.get_folder_id(drive_service, args.old_folder, args.verbose)
+    if None == old_id:
+        print('old_folder not found')
+        sys.exit(1)
+    new_id = utils.get_folder_id(drive_service, args.new_folder, args.verbose)
+    if None == new_id:
+        print('new_folder not found')
+        sys.exit(1)
+    items = utils.get_folder_contents_from_id(drive_service, old_id, args.verbose)
+    for item in items:
+        if args.verbose:
+            print('{}:{}'.format(item['id'], item['title']))
+        drive_service.files().update(fileId=item['id'],
+                                     addParents=new_id,
+                                     removeParents=old_id,
+                                     fields='id, parents').execute()
--- utils.py
+++ utils.py
@@ -12,15 +12,21 @@ def get_drive_service(tokenFile, verbose=0):
     return(apiclient.discovery.build('drive', 'v2', http=http))
 
 def get_folder_id(drive_service, folder, verbose=0):
-    q = 'title="{}" and mimeType="application/vnd.google-apps.folder"'.format(folder)
+    q = 'title="{}" and mimeType="application/vnd.google-apps.folder"'.format(
+        folder
+    )
     files = drive_service.files().list(q=q).execute()
     if 1 != len(files['items']):
-        print('did not find exactly one folder')
+        print('did not find exactly one folder, found {}'.format(
+            len(files['items'])
+        ))
         return(None)
     return(files['items'][0]['id'])
 
 def get_document_contents_from_drive(drive_service, file_id, verbose=0):
-    data = drive_service.files().export(fileId=file_id, mimeType='text/plain').execute().decode('utf8')
+    data = drive_service.files().export(
+        fileId=file_id, mimeType='text/plain'
+    ).execute().decode('utf8')
     assert(unichr(0xfeff) == data[0])
     if verbose > 0:
         print('BOM found')
@@ -29,3 +35,24 @@ def get_document_contents_from_drive(drive_service, file_id, verbose=0):
 def get_document_title_and_type_from_id(drive_service, id, verbose=0):
     data = drive_service.files().get(fileId=id).execute()
     return(data['title'], data['mimeType'])
+
+def get_folder_contents_from_id(drive_service, folder_id, verbose=0):
+    if verbose > 0:
+        print('in get_folder_contents_from_id')
+    pages_returned = 0
+    result = []
+    page_token = None
+    param = {}
+    param['q'] = "parents in '{}'".format(folder_id)
+    while True:
+        if page_token:
+            param['pageToken'] = page_token
+        files = drive_service.files().list(**param).execute()
+        result.extend(files['items'])
+        page_token = files.get('nextPageToken')
+        pages_returned += 1
+        if verbose > 0:
+            print('page {} returned'.format(pages_returned))
+        if not page_token:
+            break
+    return(result)
