76f345a3f4a3df2a8ac7ccb34e6b027c9faf1709
dj <davejagoda@users.noreply.github.com>
2017-11-08 15:49:41 -0800

    Refactor boxjwt.py

--- boxjwt.py
+++ boxjwt.py
@@ -4,16 +4,12 @@ import argparse
 import os
 from boxsdk import JWTAuth, Client
 
-def store_tokens(access_token, refresh_token):
-    pass
-
 auth = JWTAuth(
-    client_id=os.getenv('BOX_CLIENT_ID'),
-    client_secret=os.getenv('BOX_CLIENT_SECRET'),
-    enterprise_id=os.getenv('BOX_ENTERPRISE_ID'),
-    jwt_key_id=os.getenv('BOX_JWT_KEY_ID'),
-    rsa_private_key_file_sys_path=os.getenv('BOX_PRIVATE_KEY_PEM_FILE'),
-    store_tokens=store_tokens
+    client_id=os.environ['BOX_CLIENT_ID'],
+    client_secret=os.environ['BOX_CLIENT_SECRET'],
+    enterprise_id=os.environ['BOX_ENTERPRISE_ID'],
+    jwt_key_id=os.environ['BOX_JWT_KEY_ID'],
+    rsa_private_key_data=os.environ['BOX_PRIVATE_KEY_PEM']
 )
 
 if '__main__' == __name__:
@@ -26,15 +22,20 @@ if '__main__' == __name__:
     client = Client(auth)
     if args.verbose > 0:
         me = client.user(user_id='me').get()
-        print 'user_login: ' + me['login']
+        print('user_login: ' + me['login'])
     root_folder = client.folder(folder_id='0').get()
     if args.verbose > 0:
-        print 'folder owner: ' + root_folder.owned_by['login']
-        print 'folder name: ' + root_folder['name']
+        print('folder owner: ' + root_folder.owned_by['login'])
+        print('folder name: ' + root_folder['name'] + root_folder['id'])
     if args.make_folder:
         root_folder.create_subfolder(args.make_folder)
     if args.upload_file:
         root_folder.upload(args.upload_file)
     items = client.folder(folder_id='0').get_items(limit=100, offset=0)
     for item in items:
-        print item
+        assert(item.type in ['file', 'folder'])
+        if 'folder' == item.type:
+            symbol = '+'
+        else:
+            symbol = '-'
+        print('{} id:{} name:{}'.format(symbol, item.id, item.name))
